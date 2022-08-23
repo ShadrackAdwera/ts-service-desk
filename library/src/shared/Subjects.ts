83694029fc748989ed438ec1a4051bc09804993b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-08-23 16:31:20 +0300

    create subjects enum

---
+++ library/src/shared/Subjects.ts
@@ -0,0 +1,13 @@
+export enum Subjects {
+  UserCreated = 'user:created',
+  UserUpdated = 'user:updated',
+  UserDeleted = 'user:deleted',
+  CategoryCreated = 'category:created',
+  CategoryUpdated = 'category:updated',
+  CategoryDeleted = 'category:deleted',
+  TicketCreated = 'ticket:created',
+  TicketUpdated = 'ticket:updated',
+  TicketDeleted = 'ticket:deleted',
+  GroupCreated = 'group:created',
+  GroupUpdated = 'group:updated',
+}

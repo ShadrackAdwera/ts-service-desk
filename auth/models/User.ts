e0e8b077beed737ca9b2c33c9f221ec2c8e3f594
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-01 18:28:31 +0300

    modify user model

--- auth/models/User.ts
+++ auth/models/User.ts
@@ -2,37 +2,45 @@ import { Roles } from '@adwesh/service-desk';
 import { Schema, Model, Document, model } from 'mongoose';
 import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
-interface IUserDoc extends Document {
+interface UserDoc extends Document {
   username: string;
   email: string;
   password: string;
+  section: string;
+  category: string;
   resetToken?: string;
   tokenExpirationDate?: Date;
-  role: Roles;
+  roles: string[];
   version: number;
 }
 
-interface IUserModel extends Model<IUserDoc> {
+interface UserModel extends Model<UserDoc> {
   username: string;
   email: string;
   password: string;
+  section: string;
+  category: string;
   resetToken?: string;
   tokenExpirationDate?: Date;
-  role: Roles;
+  roles: string[];
 }
 
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, min: 6 },
+    // group: { type: Schema.Types.ObjectId, required: true, ref: 'group' }, might change this to also include the group a user belongs to
     resetToken: { type: String },
     tokenExpirationDate: { type: Date },
-    role: { type: String, required: true, enum: Object.values(Roles) },
+    roles: [{ type: String, enum: Object.keys(Roles), default: Roles.USER }],
   },
   { timestamps: true, toJSON: { getters: true } }
 );
 
+userSchema.set('versionKey', 'version');
 userSchema.plugin(updateIfCurrentPlugin);
-const User = model<IUserDoc, IUserModel>('user', userSchema);
+
+const User = model<UserDoc, UserModel>('user', userSchema);
+
 export { User };

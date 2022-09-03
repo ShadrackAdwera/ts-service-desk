0bd1ddfc62cd4696d2aea3b716a7a2112ed0ffb8
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-03 10:52:45 +0300

    create user model in groups service

--- groups/models/GroupsUser.ts
+++ groups/models/GroupsUser.ts
@@ -1,28 +1,58 @@
 import { Document, Model, Schema, model } from 'mongoose';
 import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { Roles } from '@adwesh/service-desk';
 
 interface GroupDoc extends Document {
   title: string;
-  agents: string[];
+  createdBy: string;
+  users: string[];
   version: number;
 }
 
 interface GroupModel extends Model<GroupDoc> {
   title: string;
-  agents: string[];
+  createdBy: string;
+  users: string[];
 }
+
+interface UserDoc extends Document {
+    userId: string;
+    email: string;
+    roles: string[];
+    version: number;
+  }
+  
+  interface UserModel extends Model<UserDoc> {
+    userId: string;
+    email: string;
+    roles: string[];
+  }
 
 const groupSchema = new Schema(
   {
     title: { type: String, required: true },
-    agents: [{ type: Schema.Types.ObjectId }],
+    createdBy: { type: Schema.Types.ObjectId },
+    users: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   },
   { timestamps: true, toJSON: { getters: true } }
 );
 
+const userSchema = new Schema(
+    {
+      userId: { type: String, required: true },
+      email: { type: String, required: true },
+      roles: [{ type: String, enum: Object.values(Roles), default: Roles.AGENT }],
+    },
+    { timestamps: true, toJSON: { getters: true } }
+  );
+
 groupSchema.set('versionKey', 'version');
 groupSchema.plugin(updateIfCurrentPlugin);
+userSchema.set('versionKey', 'version');
+userSchema.plugin(updateIfCurrentPlugin);
 
 const Group = model<GroupDoc, GroupModel>('group', groupSchema);
+const User = model<UserDoc, UserModel>('user', userSchema);
 
 export { Group };
+export { User };

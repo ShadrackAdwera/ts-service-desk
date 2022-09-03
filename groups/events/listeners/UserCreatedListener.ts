cf7e0b64ebc49df0d6798e0a054e297c9e9c8ffc
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-03 11:01:10 +0300

    configure user created listener class in group service

--- groups/events/listeners/UserCreatedListener.ts
+++ groups/events/listeners/UserCreatedListener.ts
@@ -1,15 +1,44 @@
 import { Listener, Subjects, UserCreatedEvent } from '@adwesh/service-desk';
 import { Message } from 'node-nats-streaming';
+import { HttpError } from '@adwesh/common';
+import { User } from '../../models/GroupsUser';
 
 const GROUPS_QGNAME = 'groups-service';
 
 export class UserCreatedListener extends Listener<UserCreatedEvent> {
   subject: Subjects.UserCreated = Subjects.UserCreated;
   queueGroupName: string = GROUPS_QGNAME;
-  onMessage(
+  async onMessage(
     data: { id: string; email: string; roles: string[] },
     msg: Message
-  ): void {
-    throw new Error('Method not implemented.');
+  ): Promise<void> {
+    //find user in db, if not exists, create them
+    let foundUser;
+    try {
+      foundUser = await User.findOne({ userId: data.id }).exec();
+    } catch (error) {
+      console.log(error);
+      throw new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      );
+    }
+    if (foundUser) {
+      msg.ack();
+    }
+    const newUser = new User({
+      userId: data.id,
+      email: data.email,
+    });
+    try {
+      await newUser.save();
+      msg.ack();
+    } catch (error) {
+      console.log(error);
+      throw new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      );
+    }
   }
 }

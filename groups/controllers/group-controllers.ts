7dca928addb41a17a47a6f8b6ffd8af4dd6458cf
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-05 15:48:49 +0300

    add users to group controller

--- groups/controllers/group-controllers.ts
+++ groups/controllers/group-controllers.ts
@@ -42,3 +42,30 @@ const createGroup = async (req: Request, res: Response, next: NextFunction) => {
   // publish to category
   res.status(201).json({ message: `Group ${newGroup.title} has been created` });
 };
+
+const addUsersToGroup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const groupId = req.params;
+  const { users }: { users: string[] } = req.body;
+  let foundGroup;
+  try {
+    foundGroup = await Group.findOne({ id: groupId }).exec();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  if (!foundGroup)
+    return next(new HttpError('This group does not exist!', 422));
+  for (const userId of users) {
+    foundGroup.users.push(userId);
+  }
+  //publish update to category
+  res.status(200).json({ message: 'The users have been added to this group' });
+};

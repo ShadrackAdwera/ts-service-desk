eaf83ed1978bc30e509a286a2e9617224b60ab3b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-16 12:25:44 +0300

    create assigment options enum

--- library/src/shared/AssignmentOptions.ts
+++ library/src/shared/AssignmentOptions.ts
@@ -1,5 +1,5 @@
 export enum ASSIGNMENT_OPTIONS {
   YES_ANY_USER = 'Yes, assign to any user',
   YES_SPECIFIC_USERS = 'Yes, assign to specific users',
-  NO = 'NO, manually assign',
+  NO = 'No, manually assign',
 }

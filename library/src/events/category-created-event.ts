9747d061145dad2f1ca7337bd16265b288aca741
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-16 12:31:11 +0300

    create category created event

---
+++ library/src/events/category-created-event.ts
@@ -0,0 +1,14 @@
+import { ASSIGNMENT_OPTIONS } from '../shared/AssignmentOptions';
+import { Subjects } from '../shared/Subjects';
+
+export interface CategoryCreatedEvent {
+  subject: Subjects.CategoryCreated;
+  data: {
+    id: string;
+    title: string;
+    priority: number;
+    assigmentMatrix: ASSIGNMENT_OPTIONS;
+    defaultDueDate: string;
+    groups: string[];
+  };
+}

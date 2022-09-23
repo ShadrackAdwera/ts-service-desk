b71e47ae9beec9e77d3403092fe02778ca214042
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-23 14:36:38 +0300

    configure ticket created event

---
+++ library/src/events/ticket-updated-event.ts
@@ -0,0 +1,16 @@
+import { Subjects } from '../shared/Subjects';
+
+export interface TicketUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    createdBy: string;
+    assignedTo: string;
+    status: string;
+    replies: string;
+    escalationMatrix: string;
+  };
+}

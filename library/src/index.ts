63b99ded1fa0d66ea039db2c8980d11ca41040b9
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-23 14:37:32 +0300

    config updates

--- library/src/index.ts
+++ library/src/index.ts
@@ -6,6 +6,8 @@ export * from './events/group-created-event';
 export * from './events/group-updated-event';
 export * from './events/category-created-event';
 export * from './events/category-updated-event';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
 
 export * from './shared/Subjects';
 export * from './shared/Roles';

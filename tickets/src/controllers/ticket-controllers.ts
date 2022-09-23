88c868a9382ccf204644bfeea81722af23005597
ShadrackAdwera <adweshshaddie@gmail.com>
2022-09-23 15:32:57 +0300

    create ticket controller

--- tickets/src/controllers/ticket-controllers.ts
+++ tickets/src/controllers/ticket-controllers.ts
@@ -7,7 +7,7 @@ import {
   TicketStatus,
 } from '@adwesh/service-desk';
 
-import { Ticket, Reply, Category } from '../models/Ticket';
+import { Ticket, Reply, Category, EscalationMatrix } from '../models/Ticket';
 
 const fetchTickets = async (
   req: Request,
@@ -40,4 +40,67 @@ const fetchTickets = async (
     );
   }
   res.status(200).json({ count: foundTickets.length, tickets: foundTickets });
+};
+
+const createTicket = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return next(new HttpError('Invalid inputs', 422));
+  }
+
+  const userId = req.user?.userId;
+  let foundMatrix;
+
+  if (!userId) {
+    return next(new HttpError('Invalid request', 422));
+  }
+
+  const { title, description, category } = req.body;
+
+  try {
+    foundMatrix = await EscalationMatrix.findOne({ selected: 'yes' }).exec();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+
+  if (!foundMatrix)
+    return next(
+      new HttpError(
+        'No escalation matrix configured, please contact your site administrator for assistance',
+        404
+      )
+    );
+
+  const newTicket = new Ticket({
+    title,
+    description,
+    category,
+    createdBy: userId,
+    status: TicketStatus.OPEN,
+    replies: [],
+    escalationMatrix: foundMatrix,
+  });
+
+  try {
+    await newTicket.save();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  res
+    .status(201)
+    .json({ message: 'Your ticket has been successfully raised.' });
 };

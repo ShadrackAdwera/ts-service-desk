3594cb4afe5428e97628aa19e1d018d42516c4a2
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-09 13:17:49 +0300

    update escalation controller

---
+++ escalation/src/controllers/escalation-controllers.ts
@@ -0,0 +1,71 @@
+import { validationResult } from 'express-validator';
+import { HttpError } from '@adwesh/common';
+import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose';
+import { Escalation, EscalationDoc } from '../models/Escalation';
+
+type TEscalation = EscalationDoc & { _id: Types.ObjectId };
+
+const getEscalationMatrices = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let foundMatrices: TEscalation[] = [];
+  try {
+    foundMatrices = await Escalation.find().exec();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+
+  res
+    .status(200)
+    .json({ count: foundMatrices.length, escalationMatrices: foundMatrices });
+};
+
+const updateEscalationMatrix = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const id = req.params.matrixId;
+  let foundMatrix: TEscalation | null;
+
+  const { actionTime } = req.body;
+
+  try {
+    foundMatrix = await Escalation.findById(id).exec();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+
+  if (!foundMatrix) {
+    return next(new HttpError('The provided matrix does not exist', 404));
+  }
+
+  foundMatrix.actionTime = actionTime;
+
+  try {
+    await foundMatrix.save();
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  res
+    .status(200)
+    .json({ message: `Escalation: ${foundMatrix.title} has been updated.` });
+};

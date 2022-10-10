19096f36cd836869578e0df0a7c31ae66e132534
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-10 14:52:48 +0300

    configure connection to db, nats in escalation service

--- escalation/src/index.ts
+++ escalation/src/index.ts
@@ -1,1 +1,47 @@
 // connection
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsWraper } from '@adwesh/common';
+
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO URI is not defined!');
+}
+
+if (!process.env.NATS_CLUSTER_ID) {
+  throw new Error('NATS_CLUSTER_ID must be defined');
+}
+
+if (!process.env.NATS_CLIENT_ID) {
+  throw new Error('NATS_CLIENT_ID must be defined');
+}
+
+if (!process.env.NATS_URI) {
+  throw new Error('NATS_URI must be defined');
+}
+
+const start = async () => {
+  try {
+    await natsWraper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URI!
+    );
+
+    natsWraper.client.on('close', () => {
+      console.log('NATS shutting down . . .');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWraper.client.close());
+    process.on('SIGTERM', () => natsWraper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI!);
+    app.listen(5004);
+    console.log('Connected to Escalation Service, listening on PORT: 5004');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

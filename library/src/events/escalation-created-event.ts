ba1afa45c058cc926ccf2ac5746c8d1f7e81fc24
ShadrackAdwera <adweshshaddie@gmail.com>
2022-10-10 17:42:03 +0300

    config updates

--- library/src/events/escalation-created-event.ts
+++ library/src/events/escalation-created-event.ts
@@ -1,11 +1,16 @@
 import { Subjects } from '../shared/Subjects';
 
+export interface IActions {
+  priority: string;
+  actionTime: number;
+}
+
 export interface EscalationCreatedEvent {
   subject: Subjects.EscalationMatrixCreated;
   data: {
     id: string;
     title: string;
     escalationType: string;
-    actionTime: number;
+    action: IActions[];
   };
 }
--- library/src/events/escalation-updated-event.ts
+++ library/src/events/escalation-updated-event.ts
@@ -1,9 +1,10 @@
 import { Subjects } from '../shared/Subjects';
+import { IActions } from './escalation-created-event';
 
 export interface EscalationUpdatedEvent {
   subject: Subjects.EscalationMatrixUpdated;
   data: {
     id: string;
-    actionTime: number;
+    action: IActions[];
   };
 }
